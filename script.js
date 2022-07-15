5792338ea41e4f2b1b4479016ebb49b1975e9d63
Banzyworld <102680772+Banzyworld@users.noreply.github.com>
2022-07-16 00:28:17 +0100

    added some edits

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ button.addEventListener('click',test)
 const render=(str) => {
   screen.innerText=str;
 }
+
 function test(e){
    if(e.target.tagName == 'BUTTON') {
      let val=e.target.innerText;
@@ -18,36 +19,43 @@ function test(e){
       case 'Reset':
         inputStr = '';
         break;
+
       case 'Del':
            if(inputStr.length > 0) {
           inputStr = inputStr.substring(0,inputStr.length-1);
           }
           break;
+
       case '+':
           allSymbols = '+';
           inputStr+=val
           break;
+
       case '-':
           allSymbols = '-';
           inputStr+=val
           break;
+
       case '/':
           allSymbols = '/';
           inputStr+=val
           break;
+
       case 'x':
           allSymbols = '*';
           inputStr+='*'
           break;
+
       case '=':
          let values = inputStr.split(`${allSymbols}`);
         
          inputStr= calculate(values[0],allSymbols,values[1]).toString();
           break;
+          
       default :
        inputStr+=val
      }
-     
+
     //  function to perform mathematical expression
 
     function calculate(val1,symb,val2){
